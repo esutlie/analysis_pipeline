42b00f4dd5157707f53707cf353e75080df6bceb
PastryPirate <elissasutlief@gmail.com>
2023-02-16 17:15:07 -0500

    added automated image cropping for histology

--- image_editing/__init__.py
+++ image_editing/__init__.py
@@ -1,1 +1,2 @@
 from .load_pictures import load_pictures
+from .crop_slices import crop_slices
---
+++ image_editing/align_pics.py
@@ -0,0 +1,9 @@
+from image_editing import crop_slices
+
+
+def align_pics():
+    pics = crop_slices()
+
+
+if __name__ == '__main__':
+    align_pics()
