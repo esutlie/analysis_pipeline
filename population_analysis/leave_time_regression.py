ca37e264634926f02d8742831b006328d668567c
esutlie <elissasutlief@gmail.com>
2023-02-22 10:04:17 -0500

    added pca and stuff

--- population_analysis/leave_time_regression.py
+++ population_analysis/leave_time_regression.py
@@ -23,45 +23,51 @@ def leave_time_regression():
         blocks = np.unique(session_df.block)
         blocks.sort()
         x_all = np.vstack(session_df.spike_rates.values)
-        unit_max = np.max(x_all, axis=0)
-        x_all_norm = x_all / unit_max
+        x_all_whiten, transform_function = backend.whiten(x_all)
+        # unit_max = np.max(x_all, axis=0)
+        # x_all_norm = x_all / unit_max
         y = session_df.time_from_reward.values
-        trial = session_df.trial.values
-        # fit_pca(x_all_norm, y)
+        # trial = session_df.trial.values
+        pca_on_average_pre_whitened(x_all_whiten, session_df)
+        # fit_pca(x_all_whiten, y)
         # fit_tsne(x_all_norm, y)
-        pca, transform_function, average_pca, time_bins = pca_on_average(session_df)
-        x_pca = pca.transform(transform_function(x_all))
-        fig = plt.figure(figsize=(12, 12))
-        ax = fig.add_subplot(projection='3d')
-        ax.scatter(average_pca[:, 0], average_pca[:, 1], c=time_bins, cmap="Blues_r")
-        ind = np.where((trial == 20) & (y < 2))[0]
-        ax.scatter(x_pca[ind, 0], x_pca[ind, 1], x_pca[ind, 2], c=y[ind], cmap="Reds_r")
-        plt.show()
+        # pca, transform_function, average_pca, time_bins = pca_on_average(session_df)
+        # x_pca = pca.transform(transform_function(x_all))
+        # fig = plt.figure(figsize=(12, 12))
+        # ax = fig.add_subplot(projection='3d')
+        # ax.scatter(average_pca[:, 0], average_pca[:, 1], c=time_bins, cmap="Blues_r")
+        # ind = np.where((trial == 20) & (y < 2))[0]
+        # ax.scatter(x_pca[ind, 0], x_pca[ind, 1], x_pca[ind, 2], c=y[ind], cmap="Reds_r")
+        # plt.show()
         for block in blocks:
-            block_df = session_df[session_df.block == block]
-            x = np.vstack(block_df.spike_rates.values)
-            x_block_pca = pca.transform(transform_function(x))
-            x_norm = x / unit_max
-            y = block_df.time_from_reward.values
+            # block_df = session_df[session_df.block == block]
+            # x = np.vstack(block_df.spike_rates.values)
+            # x_whiten = transform_function(x)
+            x_block_whiten = x_all_whiten[session_df.block == block]
+            # x_block_pca = pca.transform(transform_function(x))
+            # x_norm = x / unit_max
+            y_block = y[session_df.block == block]
             regressor = SVR(kernel='rbf')
             # cv = LeaveOneOut()
             # regressor = LinearRegression()
             # scores = cross_val_score(regressor, x_norm, y, scoring='neg_mean_absolute_error',
             #                          cv=cv, n_jobs=-1)
             # rmse = np.sqrt(np.mean(np.absolute(scores)))
-            regressor.fit(x_block_pca, y)
-            session_df['predict_time_from_reward'] = regressor.predict(x_pca)
+
+            regressor.fit(x_block_whiten, y_block)
+            session_df['predict_time_from_reward'] = regressor.predict(x_all_whiten)
             compare_plot(session_df, 'time_from_reward', 'predict_time_from_reward', title=f'{session}_{block}')
 
 
-def compare_plot(mouse_df, key1, key2, mouse=None, title=None, save_plot=False):
+def compare_plot(session_df, key1, key2, mouse=None, title=None, save_plot=False):
     color_sets = backend.get_color_sets()
     fig, ax = plt.subplots(1, 1, figsize=[8, 6])
-    sns.scatterplot(mouse_df[key1], mouse_df[key2], hue='block', data=mouse_df, palette='Set2')
-    blocks = mouse_df.block.unique()
+    sns.scatterplot(session_df, x=key1, y=key2, hue='block', palette='Set2')
+    # sns.scatterplot(mouse_df[key1], mouse_df[key2], hue='block', data=mouse_df, palette='Set2')
+    blocks = session_df.block.unique()
     blocks.sort()
     for i, block in enumerate(blocks):
-        block_df = mouse_df[mouse_df.block == block]
+        block_df = session_df[session_df.block == block]
         regression = LinearRegression()
         regression.fit(np.expand_dims(block_df[key1].values, axis=1), np.expand_dims(block_df[key2].values, axis=1))
         x = np.linspace(0, block_df[key1].max())
@@ -69,6 +75,7 @@ def compare_plot(mouse_df, key1, key2, mouse=None, title=None, save_plot=False):
         ax.plot(x, y, '-', c=color_sets['set2_med_dark'][i])
     ax.set_xlabel('Actual Time')
     ax.set_ylabel('Predicted Time')
+    ax.set_aspect('equal', adjustable='box')
     if title:
         ax.set_title(title)
     plt.show()
@@ -91,6 +98,18 @@ def pca_on_average(session_df):
     plt.scatter(x_pca[:40, 0], x_pca[:40, 1], c=np.array(time_bins)[:40] / 10, cmap="plasma")
     plt.show()
     return pca, transform_function, x_pca[:40], np.array(time_bins)[:40] / 10
+
+
+def pca_on_average_pre_whitened(x_whitened, session_df):
+    time_bins = range(round(session_df.time_from_reward.max() * 10))
+    x_whitened_mean = np.vstack([np.mean(np.vstack(x_whitened[(session_df.time_from_reward > t / 10) & (
+            session_df.time_from_reward < t / 10 + .1)]), axis=0) for t in time_bins])
+    pca = PCA(5)
+    pca.fit(x_whitened_mean)
+    x_pca = pca.transform(x_whitened_mean)
+    plt.scatter(x_pca[:40, 0], x_pca[:40, 1], c=np.array(time_bins)[:40] / 10, cmap="plasma")
+    plt.show()
+    # return pca, transform_function, x_pca[:40], np.array(time_bins)[:40] / 10
 
 
 def tsne_on_average(session_df):
